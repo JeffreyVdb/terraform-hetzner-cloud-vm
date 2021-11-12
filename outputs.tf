f9b9d0bb8a475692fee01c2dc353499ea40a07ad
Jeffrey Vandenborne <jeffrey.vandenborne@softwareag.com>
2021-11-12 14:09:30 +0100

    chore: add ip addresses to output variables

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "vm_name" {
-  value = hcloud_server.this.name
+output "vm" {
+  value = {
+    name         = hcloud_server.this.name
+    ipv4_address = hcloud_server.this.ipv4_address
+    ipv6_address = hcloud_server.this.ipv6_address
+  }
 }
